be302d7c76ac1d0350df8e81b02f0e30dff15c62
kabilan-7-v <kabilanvelmani@gmail.com>
2025-09-14 10:39:16 +0530

    changes

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import GangaFoundationLandingPage from './App'
-import Update from './Update'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import GangaFoundationLandingPage from './App';
+import Update from './Update';
 
 function Router() {
   return (
     <Routes>
-        <Route path="/" element={<GangaFoundationLandingPage />} />
-        <Route path="/ganga" element={<Update></Update>}/>
-      
-      </Routes>
-  )
+      <Route path="/" element={<GangaFoundationLandingPage />} />
+      <Route path="/ganga" element={<Update />} />
+    </Routes>
+  );
 }
 
-export default Router
+export default Router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import './index.css'
+import './index.css';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
 import Router from './Router';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <Router></Router>
+    <Router />
   </BrowserRouter>
 );
